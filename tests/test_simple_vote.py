18ad4480b07cf501ddc4d6b0874fe2a8afa03b13
James Campbell <james.campbell@tanti.org.uk>
2022-02-03 21:26:51 +0100

    Add test to ensure non-owner cannot whitelist an address

--- tests/test_simple_vote.py
+++ tests/test_simple_vote.py
@@ -1,3 +1,4 @@
+import brownie
 from brownie import SimpleVote, accounts
 from web3 import Web3
 
@@ -10,3 +11,14 @@ def test_add_to_whitelist():
     tx = simple_vote.whitelistAddress(voter, {"from": owner, "value": funding_amount})
     tx.wait(1)
     assert voter.balance() == funding_amount
+
+
+def test_add_to_whitelist_with_wrong_acccount():
+    owner = accounts[0]
+    voter = accounts.add()
+    funding_amount = Web3.toWei(0.01, "ether")
+    simple_vote = SimpleVote.deploy(funding_amount, {"from": owner})
+    with brownie.reverts():
+        simple_vote.whitelistAddress(
+            voter, {"from": accounts[1], "value": funding_amount}
+        )
