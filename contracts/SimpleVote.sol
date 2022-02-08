4dff4949535d49eb5374cdc0a344fd8790c0c3ae
James Campbell <james.campbell@tanti.org.uk>
2022-02-08 09:36:29 +0100

    Use openzeppelin Roles for whitelist

--- contracts/SimpleVote.sol
+++ contracts/SimpleVote.sol
@@ -2,17 +2,20 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract SimpleVote is Ownable {
+contract SimpleVote is Ownable, AccessControl {
+    bytes32 public constant REGISTERED_VOTER = keccak256("REGISTERED_VOTER");
     uint256 fundingAmount;
-    mapping(address => bool) private registeredVoters;
+    mapping(uint256 => uint256) public votes;
 
     constructor(uint256 _fundingAmount) {
+        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
         fundingAmount = _fundingAmount;
     }
 
     function whitelistAddress(address payable user) public payable onlyOwner {
-        registeredVoters[user] = true;
+        grantRole(REGISTERED_VOTER, user);
         user.transfer(fundingAmount);
     }
 }
--- tests/test_simple_vote.py
+++ tests/test_simple_vote.py
@@ -22,3 +22,16 @@ def test_add_to_whitelist_with_wrong_acccount():
         simple_vote.whitelistAddress(
             voter, {"from": accounts[1], "value": funding_amount}
         )
+
+
+# def test_whitelist_user_can_vote():
+#     owner = accounts[0]
+#     voter = accounts.add()
+#     funding_amount = Web3.toWei(0.01, "ether")
+#     simple_vote = SimpleVote.deploy(funding_amount, {"from": owner})
+#     tx = simple_vote.whitelistAddress(voter, {"from": owner, "value": funding_amount})
+#     tx.wait(1)
+#     candidate = "A"
+#     tx = simple_vote.vote(candidate, {"from": voter})
+#     tx.wait(1)
+#     assert simple_vote.getVote(voter) == "A"
