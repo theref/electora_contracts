ac3ed64c83817716a5b67babb0932c7b2ccc5348
James Campbell <james.campbell@tanti.org.uk>
2022-02-08 10:26:18 +0100

    Remove funding amount

--- contracts/SimpleVote.sol
+++ contracts/SimpleVote.sol
@@ -6,23 +6,21 @@ import "@openzeppelin/contracts/access/AccessControl.sol";
 
 contract SimpleVote is Ownable, AccessControl {
     bytes32 public constant REGISTERED_VOTER = keccak256("REGISTERED_VOTER");
-    uint256 fundingAmount;
     uint256[] public votes;
 
-    constructor(uint256 _fundingAmount, uint256 _num_candidates) {
+    constructor(uint256 _num_candidates) {
         _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
-        fundingAmount = _fundingAmount;
         votes = new uint256[](_num_candidates);
     }
 
     function whitelistAddress(address payable user) public payable onlyOwner {
         grantRole(REGISTERED_VOTER, user);
-        user.transfer(fundingAmount);
     }
 
-    function vote(uint256 candidate) public {
+    function vote(uint256 candidate) public payable {
         require(hasRole(REGISTERED_VOTER, msg.sender));
         votes[candidate]++;
+
         renounceRole(REGISTERED_VOTER, msg.sender);
     }
 }
--- tests/test_simple_vote.py
+++ tests/test_simple_vote.py
@@ -6,28 +6,24 @@ from web3 import Web3
 def test_add_to_whitelist():
     owner = accounts[0]
     voter = accounts.add()
-    funding_amount = Web3.toWei(0.01, "ether")
-    simple_vote = SimpleVote.deploy(funding_amount, 0, {"from": owner})
-    tx = simple_vote.whitelistAddress(voter, {"from": owner, "value": funding_amount})
+    simple_vote = SimpleVote.deploy(0, {"from": owner})
+    tx = simple_vote.whitelistAddress(voter, {"from": owner})
     tx.wait(1)
-    assert voter.balance() == funding_amount
+    assert simple_vote.hasRole(Web3.keccak(text="REGISTERED_VOTER"), voter)
+    assert not simple_vote.hasRole(Web3.keccak(text="REGISTERED_VOTER"), accounts[1])
 
 
 def test_add_to_whitelist_with_wrong_acccount():
     owner = accounts[0]
     voter = accounts.add()
-    funding_amount = Web3.toWei(0.01, "ether")
-    simple_vote = SimpleVote.deploy(funding_amount, 0, {"from": owner})
+    simple_vote = SimpleVote.deploy(0, {"from": owner})
     with brownie.reverts():
-        simple_vote.whitelistAddress(
-            voter, {"from": accounts[1], "value": funding_amount}
-        )
+        simple_vote.whitelistAddress(voter, {"from": accounts[1]})
 
 
 def test_create_candidate_array():
     owner = accounts[0]
-    funding_amount = Web3.toWei(0.01, "ether")
-    simple_vote = SimpleVote.deploy(funding_amount, 2, {"from": owner})
+    simple_vote = SimpleVote.deploy(2, {"from": owner})
     assert simple_vote.votes(0) == 0
     assert simple_vote.votes(1) == 0
     with brownie.reverts():
@@ -37,9 +33,8 @@ def test_create_candidate_array():
 def test_vote_for_candidate():
     owner = accounts[0]
     voter = accounts.add()
-    funding_amount = Web3.toWei(0.01, "ether")
-    simple_vote = SimpleVote.deploy(funding_amount, 2, {"from": owner})
-    tx = simple_vote.whitelistAddress(voter, {"from": owner, "value": funding_amount})
+    simple_vote = SimpleVote.deploy(2, {"from": owner})
+    tx = simple_vote.whitelistAddress(voter, {"from": owner})
     tx.wait(1)
     tx = simple_vote.vote(0, {"from": voter})
     tx.wait(1)
@@ -49,9 +44,8 @@ def test_vote_for_candidate():
 def test_cannot_vote_twice():
     owner = accounts[0]
     voter = accounts.add()
-    funding_amount = Web3.toWei(0.01, "ether")
-    simple_vote = SimpleVote.deploy(funding_amount, 2, {"from": owner})
-    tx = simple_vote.whitelistAddress(voter, {"from": owner, "value": funding_amount})
+    simple_vote = SimpleVote.deploy(2, {"from": owner})
+    tx = simple_vote.whitelistAddress(voter, {"from": owner})
     tx.wait(1)
     tx = simple_vote.vote(0, {"from": voter})
     tx.wait(1)
