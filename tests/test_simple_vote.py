5aab0b0c2efd1b3074c58667d0626e5fe7f996a5
James Campbell <james.campbell@tanti.org.uk>
2022-05-16 15:26:24 +0300

    Change from solidity & brownie to vyper & ape

--- tests/test_simple_vote.py
+++ tests/test_simple_vote.py
@@ -1,55 +1,47 @@
-import brownie
-import pytest
-from brownie import SimpleVote, accounts, exceptions
-from web3 import Web3
+import ape
 
 
-def test_add_to_whitelist():
-    owner = accounts[0]
-    voter = accounts.add()
-    simple_vote = SimpleVote.deploy(0, {"from": owner})
-    tx = simple_vote.whitelistAddress(voter, {"from": owner})
-    tx.wait(1)
-    assert simple_vote.hasRole(Web3.keccak(text="REGISTERED_VOTER"), voter)
-    assert not simple_vote.hasRole(Web3.keccak(text="REGISTERED_VOTER"), accounts[1])
+def test_add_candidate(owner, candidate, project):
+    simple_vote = owner.deploy(project.SimpleVote)
+    simple_vote.add_candidate(candidate, "New Candidate", sender=owner)
+    assert simple_vote.candidates(candidate).name == "New Candidate"
+    assert simple_vote.candidates(candidate).vote_count == 0
 
 
-def test_add_to_whitelist_with_wrong_acccount():
-    owner = accounts[0]
-    voter = accounts.add()
-    simple_vote = SimpleVote.deploy(0, {"from": owner})
-    with pytest.raises(exceptions.VirtualMachineError):
-        simple_vote.whitelistAddress(voter, {"from": accounts[1]})
+def test_register_voter(owner, voter, random, project):
+    simple_vote = owner.deploy(project.SimpleVote)
+    simple_vote.register_voter(voter, sender=owner)
+    assert simple_vote.voters(voter).voted is False
+    assert simple_vote.voters(voter).weight == 1
+    assert simple_vote.voters(random).weight == 0
 
 
-def test_create_candidate_array():
-    owner = accounts[0]
-    simple_vote = SimpleVote.deploy(2, {"from": owner})
-    assert simple_vote.votes(0) == 0
-    assert simple_vote.votes(1) == 0
-    with pytest.raises(exceptions.VirtualMachineError):
-        simple_vote.votes(2)
+def test_register_voter_not_owner(owner, voter, project):
+    simple_vote = owner.deploy(project.SimpleVote)
+    with ape.reverts():
+        simple_vote.register_voter(voter, sender=voter)
 
 
-def test_vote_for_candidate():
-    owner = accounts[0]
-    voter = accounts.add()
-    simple_vote = SimpleVote.deploy(2, {"from": owner})
-    tx = simple_vote.whitelistAddress(voter, {"from": owner})
-    tx.wait(1)
-    tx = simple_vote.vote(0, {"from": voter})
-    tx.wait(1)
-    assert simple_vote.votes(0) == 1
+def test_vote(owner, voter, candidate, project):
+    simple_vote = owner.deploy(project.SimpleVote)
+    simple_vote.register_voter(voter, sender=owner)
+    simple_vote.add_candidate(candidate, "New Candidate", sender=owner)
+    simple_vote.vote(candidate, sender=voter)
+    assert simple_vote.candidates(candidate).vote_count == 1
+    assert simple_vote.voters(voter).voted is True
 
 
-def test_cannot_vote_twice():
-    owner = accounts[0]
-    voter = accounts.add()
-    simple_vote = SimpleVote.deploy(2, {"from": owner})
-    tx = simple_vote.whitelistAddress(voter, {"from": owner})
-    tx.wait(1)
-    tx = simple_vote.vote(0, {"from": voter})
-    tx.wait(1)
-    with pytest.raises(exceptions.VirtualMachineError):
-        tx = simple_vote.vote(0, {"from": voter})
-        tx.wait(1)
+def test_vote_not_registerd(owner, random, candidate, project):
+    simple_vote = owner.deploy(project.SimpleVote)
+    simple_vote.add_candidate(candidate, "New Candidate", sender=owner)
+    with ape.reverts():
+        simple_vote.vote(candidate, sender=random)
+
+
+def test_cannot_vote_twice(owner, voter, candidate, project):
+    simple_vote = owner.deploy(project.SimpleVote)
+    simple_vote.register_voter(voter, sender=owner)
+    simple_vote.add_candidate(candidate, "New Candidate", sender=owner)
+    simple_vote.vote(candidate, sender=voter)
+    with ape.reverts():
+        simple_vote.vote(candidate, sender=voter)
