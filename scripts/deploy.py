8de05da83a3d6902b64265d9a38cc76d5d116687
James Campbell <james.campbell@tanti.org.uk>
2022-05-16 13:26:06 +0000

    Revert "Change from solidity & brownie to vyper & ape"
    This reverts commit bc4cb912b8574d2f5f66cf1b33a90d1cb6bb90bc

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -5,10 +5,26 @@
 #
 # This software is published at https://github.com/theref/electora
 
-# import ape
-from ape import accounts, networks, project
+from brownie import SimpleStorage, accounts, config, network
+
+
+def deploy_simple_storage():
+    account = get_account()
+    simple_storage = SimpleStorage.deploy({"from": account})
+    stored_value = simple_storage.retrieve()
+    print(stored_value)
+    transaction = simple_storage.store(15, {"from": account})
+    transaction.wait(1)
+    updated_stored_value = simple_storage.retrieve()
+    print(updated_stored_value)
+
+
+def get_account():
+    if (network.show_active) == "development":
+        return accounts[0]
+    else:
+        return accounts.add(config["wallets"]["from_key"])
 
 
 def main():
-    account = accounts.load("MetaMaskTest2")
-    return account.deploy(project.SimpleVote)
+    deploy_simple_storage()
